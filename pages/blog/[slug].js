6af5cb85fb1b5b1f4ca95e9b0f2bd4dc1b18eb15
vpatsyuk <vpacyuk@team.amocrm.com>
2023-03-29 16:22:05 +0300

    styling for paragraph

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,9 +1,48 @@
-export default function BlogPage({ slug }) {
-	return <h1>Hello {slug}</h1>
+import { getBlogPage } from '@/src/contentful'
+import styled from '@emotion/styled'
+import { Box } from '@mui/system'
+
+import MuiMarkdown from 'mui-markdown'
+
+const MarkdownImage = styled.img`
+	max-width: 100%;
+`
+
+const overrides = {
+	img: ({ alt, src }) => (
+		<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+			<MarkdownImage alt={alt} src={`${src}?q=80&h=600`} />
+		</Box>
+	),
 }
 
-export function getServerSideProps({ query: { slug } }) {
+export default function BlogPage({ content, ...props }) {
+	return (
+		<Box sx={{ maxWidth: '1440px', width: '100%', margin: '0 auto' }}>
+			{content.map((item, key) => {
+				const contentType = item.sys.contentType.sys.id
+
+				if (contentType === 'paragraph') {
+					return (
+						<MuiMarkdown overrides={overrides} key={key}>
+							{item.fields.text}
+						</MuiMarkdown>
+					)
+				}
+				return (
+					<span key={key}>
+						<br />
+						{`Unknown type ${contentType}`}
+					</span>
+				)
+			})}
+		</Box>
+	)
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+	const blog = await getBlogPage(slug)
 	return {
-		props: { slug },
+		props: { ...blog.fields },
 	}
 }
